8400fd64f5bc430bc5f83a42ebba563941b8bbad
Tvopi <payko-v@bk.ru>
2023-03-08 13:26:33 +0800

    Добавление отработки кликов  и реализация  функционала mark done.

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,13 +1,26 @@
 <template>
   <q-page class="bg-yellow-5 column">
     <q-list separator bordered>
-      <q-item v-for="task in tasks" :key="task.title" tag="label" v-ripple>
+      <q-item
+        @click="task.done = !task.done"
+        clickable
+        :class="{ 'done bg-blue-1': task.done }"
+        v-for="task in tasks"
+        :key="task.title"
+        tag="label"
+        v-ripple
+      >
         <q-item-section avatar>
-          <q-checkbox v-model="task.done" val="teal"/>
+          <q-checkbox
+            class="no-pointer-events"
+            v-model="task.done"
+            val="teal"
+          />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
         </q-item-section>
+        <q-item-section v-if="task.done" side> x </q-item-section>
       </q-item>
     </q-list>
     <div class="flex flex-center">
@@ -34,15 +47,20 @@ export default {
           done: false,
         },
         {
-
           title: "Cheburashka3",
           done: false,
         },
       ],
-      
     };
-
-
   },
 };
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: pink;
+  }
+}
+</style>
