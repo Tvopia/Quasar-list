f4e61a450948d82116b2a3cea83458b58d8bd135
Tvopi <payko-v@bk.ru>
2023-03-08 12:51:23 +0800

    Использование/создание  чекбоксов, объектов

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,14 +1,48 @@
 <template>
-  <q-page class="flex flex-center bg-yellow-5">
-    <h5></h5>
-    <img
-      alt="Cheburashka"
-      src="../assets/img/stich.png"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="bg-yellow-5">
+    <q-list class="column">
+      <q-item v-for="task in tasks" :key="task.title" tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="teal" color="teal" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div class="flex flex-center">
+      <img
+        alt="Cheburashka"
+        src="../assets/img/stich.png"
+        style="width: 200px; height: 200px"
+      />
+    </div>
   </q-page>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Cheburashka",
+          done: false,
+        },
+        {
+          title: "Cheburashka2",
+          done: false,
+        },
+        {
 
+          title: "Cheburashka3",
+          done: false,
+        },
+      ],
+      
+    };
+
+
+  },
+};
 </script>
